ad8aaa67dfd292b9bbd0f9d095b20bf2b22d4489
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-25 17:17:46 +0100

    dev: docker compose with mongo connection

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 const express = require("express");
 const serveIndex = require("serve-index");
 
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://mongo/test");
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    // minlength: 3
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+const Todo = mongoose.model("Todo", schema);
+
+(async () => {
+  const todos = await Todo.find();
+  console.log("todos: ", todos);
+})();
+
 const port = 3000;
 
 const app = express();
+
+app.get("/todos", (req, res) => {
+  (async () => {
+    try {
+      const todos = await Todo.find();
+      console.log("todos: ", todos);
+      res.json(todos);
+    } catch (err) {
+      console.log("err: ", err);
+      res.status(500).end();
+    }
+  })();
+});
 
 app.use(express.static("."));
 app.use(serveIndex(".", { icons: true }));
